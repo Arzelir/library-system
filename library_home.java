b484fa13766dec5996856bee74389b8df591b3e1
Arzelir <365yaseen@gmail.com>
2024-03-04 18:57:59 -0500

    edited

--- library_home.java
+++ library_home.java
@@ -2,51 +2,40 @@ import java.util.Scanner;
 
 class library_home {
 
-    static boolean signInValue;
+    static boolean signInValue = false;
 
-    void homePage(boolean signinCheck) {
-        signInValue = signinCheck;
-    }
-
+    // clear screen method
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
     public static void main(String[] args) {
+
         // imported classes objects
         Scanner obj = new Scanner(System.in);
         visitor visit = new visitor();
+        account_center accounts = new account_center();
         signin sign = new signin();
-        admin administrator = new admin();
 
-        String username, password;
+        // make this private somehow
+        accounts.setAccount("yaseen", "qazi");
 
         // Sign in pages
-        clearScreen();
         System.out.println("Welcome to the Public Library! \nAdmin or Visitor? \n");
         String response = obj.next();
-        if (response.toLowerCase().equals("admin")) {
-            clearScreen();
-            System.out.println("Please enter username and password");
-            System.out.print("Username:  ");
-            username = obj.next();
-            System.out.print("\nPassword:  ");
-            password = obj.next();
-            sign.logIn(username, password);
-            clearScreen();
-        }
 
-        // created options setting for administrators
-        if (signInValue = true) {
-            administrator.adminOptions();
+        switch (response) {
+            case "admin":
+                sign.logIn();
+                break;
+            case "visitor":
+                visit.options();
+                break;
+            default:
+                System.out.println("Please Enter Valid Response");
+                break;
         }
-
-        else {
-            
-            visit.options();
-        }
-
         obj.close();
     }
 }
